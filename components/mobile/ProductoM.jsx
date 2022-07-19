d05821013d1b2e6a48809b933a3bfe396709ff2a
mariano barrionuevo <marianobarrionuevo@marianos-MBP.fios-router.home>
2022-07-19 14:45:40 -0400

    fixing design details

--- components/mobile/ProductoM.jsx
+++ components/mobile/ProductoM.jsx
@@ -121,7 +121,10 @@ const ProduyctoM = () => {
                         <p>
                             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                             ince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                         </p>
+                        
                     </div>
                     <p>
                         Lorem Ipsum is simply dummy text of the printing and typesetting industry.
